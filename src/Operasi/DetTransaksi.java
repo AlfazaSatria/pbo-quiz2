e6fbd0d0836b48150f33e61d214fa4bfa52c6a83
acer <acer@Alfaza>
2018-11-27 00:42:11 +0700

    Membuat class Transaksi besertsa constructor dan Setter Total yang belum ada pada Class Item

---
+++ src/Operasi/DetTransaksi.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operasi;
+
+import java.util.ArrayList;
+import java.util.*;
+/**
+ *
+ * @author Alfaza
+ */
+public class DetTransaksi {
+    private String kode; //Variabel Kode
+    private float Total; //Variabel Total
+    private ArrayList <Item> barang = new ArrayList<>(); //Variabel Item
+
+    public DetTransaksi(String kode, ArrayList<Item> barang) {
+        this.kode = kode;
+        this.barang=barang;
+    }
+    
+    //Setter Total
+    public void setTotal(){
+        float total =0;
+        for (Item items : this.barang){
+            Total += items.getTotal();
+    }
+        this.Total=Total;
+    }
+    
+    
+    
+} 
